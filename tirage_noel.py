185d04d20671bbde4c4d85ff4b8616e0d28cbc6e
Hugo Boulanger <hugo_boulanger@hotmail.fr>
2022-10-22 13:02:08 +0200

    Improve code quality

--- tirage_noel.py
+++ tirage_noel.py
@@ -3,49 +3,59 @@
 from random import *
 import time
 
-def check(attributions, interdictions):
-    for attribution in attributions.items():
-        for interdiction in interdictions:
-            if attribution[0] in interdiction and attribution[1] in interdiction:
-                return False
-    return True
+
+class SolutionException(Exception):
+    pass
+
+
+def usesForbiddenGroup(solution, groups):
+    for transaction in solution.items():
+        for group in groups:
+            if transaction[0] in group and transaction[1] in group:
+                return True
+    return False
+
+
+def tryGenerateRandomSolution():
+    gifters = set(names)
+    receivers = set(names)
+    solution = {}
+    fail = False
+    for gifter in gifters:
+        potential_receivers = set(receivers)
+        if gifter in potential_receivers:
+            potential_receivers.remove(gifter)
+        if not potential_receivers:
+            raise SolutionException
+        index = randrange(0, len(potential_receivers))
+        receiver = list(potential_receivers)[index]
+        solution[gifter] = receiver
+        receivers.remove(receiver)
+    return solution
+
+
+groups = [
+        ['Isabelle', 'Hugo', 'Nicolas', 'Lucie'],
+        ['Christian', 'Marie'],
+        ['Jean', 'Claude'],
+] 
+names = {'Isabelle', 'Hugo', 'Nicolas', 'Lucie', 'Julien', 'Claude', 'Jean', 'Marie', 'Christian'}
 
 
 def main():
-    interdictions = [
-            ['Isabelle', 'Hugo', 'Nicolas', 'Lucie'],
-            ['Christian', 'Marie'],
-            ['Jean', 'Claude'],
-    ]
     seed(time.time())
-    ok = False
-    names = {'Isabelle', 'Hugo', 'Nicolas', 'Lucie', 'Julien', 'Claude', 'Jean', 'Marie', 'Christian'}
-    while not ok:
-        gifters = set(names)
-        receivers = set(names)
-        attributions = {}
-        fail = False
-        for gifter in gifters:
-            potential_receivers = set(receivers)
-            if gifter in potential_receivers:
-                potential_receivers.remove(gifter)
-            if not potential_receivers:
-                fail = True
+    while True:
+        try:
+            solution = tryGenerateRandomSolution()
+            if not usesForbiddenGroup(solution, groups):
                 break
-            index = randrange(0, len(potential_receivers))
-            receiver = list(potential_receivers)[index]
-            attributions[gifter] = receiver
-            receivers.remove(receiver)
-        
-        if fail:
-            ok = False
-        else:
-            ok = check(attributions, interdictions)
+        except SolutionException:
+            pass
 
-    for attribution in attributions.items():
-        gifter = attribution[0].ljust(10, ' ')
-        receiver = attribution[1]
-        print(f'{gifter} Donne à {receiver}')
+    for transaction in solution.items():
+        gifter = transaction[0].ljust(10, ' ')
+        receiver = transaction[1]
+        print(f'{gifter} offre à {receiver}')
 
 
 if __name__ == "__main__":
