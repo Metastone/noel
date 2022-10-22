e91ec0b7c49e7709fa132559848d0c14b217b89a
Hugo Boulanger <hugo_boulanger@hotmail.fr>
2022-10-22 17:07:01 +0200

    Load configuration as yaml file using PyYAML and schema

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Organize a random gift exchange. Made initially to be used for Christmas with my
 - Takes a list of interdictions (groups of people that can't gift to one another)
 - No isolated cycles (if you represent the output like a graph with peoples as nodes and "giving gifts" relations as edges)
 
+# Dependencies
+
+- PyYAML (to load the program configuration)
+- "schema" python library (to validate the program configuration)
--- tirage_noel.py
+++ tirage_noel.py
@@ -1,14 +1,20 @@
 #!/bin/python3
 
 from random import *
+from schema import Schema, SchemaError
 import time
+import yaml
 
 
-class SolutionException(Exception):
+class ChristmasException(Exception):
     pass
 
 
-def usesForbiddenGroup(solution, groups):
+class BadRandomSolutionException(Exception):
+    pass
+
+
+def uses_forbidden_group(solution, groups):
     for transaction in solution.items():
         for group in groups:
             if transaction[0] in group and transaction[1] in group:
@@ -16,48 +22,70 @@ def usesForbiddenGroup(solution, groups):
     return False
 
 
-def tryGenerateRandomSolution():
-    gifters = set(names)
-    receivers = set(names)
+def try_generate_random_solution(participants):
+    givers = set(participants)
+    receivers = set(participants)
     solution = {}
-    fail = False
-    for gifter in gifters:
+    for giver in givers:
         potential_receivers = set(receivers)
-        if gifter in potential_receivers:
-            potential_receivers.remove(gifter)
+        if giver in potential_receivers:
+            potential_receivers.remove(giver)
         if not potential_receivers:
-            raise SolutionException
+            raise BadRandomSolutionException
         index = randrange(0, len(potential_receivers))
         receiver = list(potential_receivers)[index]
-        solution[gifter] = receiver
+        solution[giver] = receiver
         receivers.remove(receiver)
     return solution
 
 
-groups = [
-        ['Isabelle', 'Hugo', 'Nicolas', 'Lucie'],
-        ['Christian', 'Marie'],
-        ['Jean', 'Claude'],
-] 
-names = {'Isabelle', 'Hugo', 'Nicolas', 'Lucie', 'Julien', 'Claude', 'Jean', 'Marie', 'Christian'}
+def load_configuration():
+    # Load configuration
+    config = yaml.safe_load(open('config.yml'))
+
+    # Check that the format of the configuration is correct
+    config_schema = Schema({
+        'participants': [str],
+        'forbidden_groups': [[str]]
+    })
+    try:
+        config_schema.validate(config)
+    except SchemaError as se:
+        raise ChristmasException(f'Bad configuration : {se}')
+
+    # Check that the names in 'forbidden groups' are correct (to avoid spelling mistakes...)
+    participants = config['participants']
+    forbidden_groups = config['forbidden_groups']
+    for group in forbidden_groups:
+        for name in group:
+            if name not in participants:
+                raise ChristmasException(f'Bad configuration : {name} is mentioned in the forbidden groups, '
+                                         f'but this person is not one of the participants')
+
+    # If we reach this point, the configuration is valid
+    return config['participants'], config['forbidden_groups']
 
 
 def main():
+    (participants, forbidden_groups) = load_configuration()
     seed(time.time())
     while True:
         try:
-            solution = tryGenerateRandomSolution()
-            if not usesForbiddenGroup(solution, groups):
+            solution = try_generate_random_solution(participants)
+            if not uses_forbidden_group(solution, forbidden_groups):
                 break
-        except SolutionException:
+        except BadRandomSolutionException:
+            # random generation failed, we have to try again, nothing to do here
             pass
 
     for transaction in solution.items():
-        gifter = transaction[0].ljust(10, ' ')
+        giver = transaction[0].ljust(10, ' ')
         receiver = transaction[1]
-        print(f'{gifter} offre à {receiver}')
+        print(f'{giver} --> {receiver}')
 
 
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    try:
+        main()
+    except ChristmasException as ce:
+        print(f'ERROR : {ce}')
